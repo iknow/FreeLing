83a57b4033470e0eb05f9cef725dec49f14da57e
lluisp <lluisp@9b274142-39a2-40f6-a478-1b104364ee40>
2009-07-17 11:53:44 +0000

    git-svn-id: http://devel.cpl.upc.edu/freeling/svn/latest/freeling@294 9b274142-39a2-40f6-a478-1b104364ee40

--- src/utilities/compile_ukb.cc
+++ src/utilities/compile_ukb.cc
@@ -12,15 +12,17 @@ using namespace ukb;
 
 int main(int argc, char *argv[]) {
 
+  if (argc!=3) {
+    cerr<<"Usage: compile_ukb infile outfile"<<endl;
+    exit(1);
+  }  
+
   glVars::kb::v1_kb = false; // Use v2 format
   glVars::kb::filter_src = false; // by default, don't filter relations by src
 
   set<string> filter;
-
-  cout<<"Loading file "<<string(argv[1])<<" to "<<argv[2]<<endl;
   Kb::create_from_txt(argv[1], filter);
   Kb::instance().add_comment("Created by FreeLing installer with compile_ukb");
-  cout<<"Writting file "<<string(argv[2])<<endl;
   Kb::instance().write_to_binfile(argv[2]);
 }
 
