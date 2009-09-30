1c087073c028fff9315208e0ef2b00f1e256128c
manuelg <manuelg@9b274142-39a2-40f6-a478-1b104364ee40>
2009-09-30 08:49:51 +0000

    Actualización con el corrector ortografico
    git-svn-id: http://devel.cpl.upc.edu/freeling/svn/latest/freeling@353 9b274142-39a2-40f6-a478-1b104364ee40

--- src/main/sample_analyzer/analyzer_server.cc
+++ src/main/sample_analyzer/analyzer_server.cc
@@ -673,7 +673,8 @@ int main (int argc, char **argv) {
 			      cfg.MACO_QuantitiesDetection,
 			      cfg.MACO_DictionarySearch,
 			      cfg.MACO_ProbabilityAssignment,
-			      cfg.MACO_NER_which);
+			      cfg.MACO_NER_which,
+			      cfg.MACO_OrthographicCorrection);
       // decimal/thousand separators used by number detection
       opt.set_nummerical_points (cfg.MACO_Decimal, cfg.MACO_Thousand);
       // Minimum probability for a tag for an unkown word
@@ -683,7 +684,7 @@ int main (int argc, char **argv) {
       opt.set_data_files (cfg.MACO_LocutionsFile, cfg.MACO_QuantitiesFile,
 			  cfg.MACO_AffixFile, cfg.MACO_ProbabilityFile,
 			  cfg.MACO_DictionaryFile, cfg.MACO_NPdataFile,
-			  cfg.MACO_PunctuationFile);
+			  cfg.MACO_PunctuationFile,cfg.MACO_CorrectorLang,cfg.MACO_CorrectorCommon);
       // create analyzer with desired options
       morfo = new maco (opt);
   }
