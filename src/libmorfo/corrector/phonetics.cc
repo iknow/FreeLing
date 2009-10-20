0e273c2a38d7808e33390369c033a309dda1c0dc
lluisp <lluisp@9b274142-39a2-40f6-a478-1b104364ee40>
2009-10-20 15:43:31 +0000

    git-svn-id: http://devel.cpl.upc.edu/freeling/svn/latest/freeling@374 9b274142-39a2-40f6-a478-1b104364ee40

--- src/libmorfo/corrector/phonetics.cc
+++ src/libmorfo/corrector/phonetics.cc
@@ -48,9 +48,9 @@ using namespace std;
 /////////////////////////////////////////////////////////////////////////////
 
 string chomp (string e){
-	string s=e;
-	s.erase(s.size()); 
-	return s;
+  string s=e;
+  s.erase(s.size()); 
+  return s;
 }
 
 
@@ -60,97 +60,96 @@ string chomp (string e){
 
 phonetics::phonetics(string rulesFileIn, string dictFileIn, string sampaFileIn, bool useDictIn) {
 
-	string rulesFile=rulesFileIn;
-	string dictFile=dictFileIn;
-	string sampaFile=sampaFileIn;
-	useDict=useDictIn;
+  string rulesFile=rulesFileIn;
+  string dictFile=dictFileIn;
+  string sampaFile=sampaFileIn;
+  useDict=useDictIn;
+  
+  
+  // loading rules
+  TRACE(3,"loading rules from "+rulesFile);
+  ifstream SC;
+  SC.open(rulesFile.c_str());
+  if(!SC) {   ERROR_CRASH("Error opening file "+rulesFile);}
+  
+  string line;
+  RegEx re0("^\\*");
+  RegEx re1("(^\\s[\t]*$)");
+  RegEx re2("(^(.)=(.+)$)");
+  RegEx re3("(^[^\\/]+\\/[^\\/]*\\/.+$)");
+  
+  while(getline(SC, line)) {
+    if (re0.Search(line)) continue; 
+    if (re1.Search(line)) continue;  
+    chomp(line); 
+    if (re2.Search(line)) {
+      vars[re2.Match(2)]=re2.Match(3); 
+    }
+    else if (re3.Search(line)) { 
+      rules.push_back(line); 
+    }
+  }
+  
+  SC.close();
+  
+  // loading the dictionary
+  
+  if (useDict)
+    {
+      TRACE(3,"loading dictionary from "+dictFile);
+      ifstream D;
+      D.open(dictFile.c_str()); 
+      if(!D) { ERROR_CRASH("Error opening file "+dictFile);}
+      RegEx re4("(^\\#)");
+      RegEx re5("(^$)");
 
-
-	// loading rules
-	TRACE(3,"loading rules from "+rulesFile);
-	ifstream SC;
-	SC.open(rulesFile.c_str());
-	if(!SC) {   ERROR_CRASH("Error opening file "+rulesFile);}
-
-	string line;
-	RegEx re0("^\\*");
-	RegEx re1("(^\\s[\t]*$)");
-	RegEx re2("(^(.)=(.+)$)");
-	RegEx re3("(^[^\\/]+\\/[^\\/]*\\/.+$)");
+      vector<string> vs;
+      
+      while(getline(D, line)) {
+	chomp(line);
+	if (re4.Search(line)) continue; 
+	if (re5.Search(line)) continue; 
 	
-	while(getline(SC, line)) {
-		if (re0.Search(line)) continue; 
-		if (re1.Search(line)) continue;  
-		chomp(line); 
-		if (re2.Search(line)) {
-			vars[re2.Match(2)]=re2.Match(3); 
-		}
-		else if (re3.Search(line)) { 
-			rules.push_back(line); 
-		}
-	}
-
-	SC.close();
-
-	// loading the dictionary
+	vs=util::string2vector(line," ");
+	DIC[vs.at(0)]=vs.at(1);
+      }
+      D.close();
+    }
+  
+  
+  // loading sampa file
+  TRACE(3,"loading sampa file from "+sampaFile);
+  
+  ifstream S;
+  S.open(sampaFile.c_str()); 
+  if(!S) { ERROR_CRASH("Error opening file "+sampaFile);}
+  RegEx re6("(^\\#)");
+  RegEx re7("(^$)");
+  vector<string> vs2,vr1,vr2;
+  string r1,r2;
+  
+  while(getline(S, line)){
+    // File format: CMU \t SC SAMPA asciiN
+    chomp(line);
+    if (re6.Search(line)) continue;
+    if (re7.Search(line)) continue;
+    vs2=util::string2vector(line,"\t");
+    if (vs2.size()>2){
+      vr1=util::string2vector(vs2.at(1),"/");
+      if (vr1.size()>0) r1=vr1.at(0);
+      vr2=util::string2vector(vs2.at(2),"/");
+      if (vr2.size()>0) r2=vr2.at(0);
+      if ((vr2.size()>0) and (vr1.size()>0)){
+	SAMPA[r1]=r2;
 	
-	if (useDict)
-	{
-		TRACE(3,"loading dictionary from "+dictFile);
-		ifstream D;
-		D.open(dictFile.c_str()); 
-		if(!D) { ERROR_CRASH("Error opening file "+dictFile);}
-		RegEx re4("(^\\#)");
-		RegEx re5("(^$)");
-
-		vector<string> vs;
-
-		while(getline(D, line)) {
-			chomp(line);
-			if (re4.Search(line)) continue; 
-			if (re5.Search(line)) continue; 
-	
-			vs=util::string2vector(line," ");
-			DIC[vs.at(0)]=vs.at(1);
-			
-		}
-		D.close();
-	}
-
-
-	// loading sampa file
-	TRACE(3,"loading sampa file from "+sampaFile);
-
-	ifstream S;
-	S.open(sampaFile.c_str()); 
-	if(!S) { ERROR_CRASH("Error opening file "+sampaFile);}
-	RegEx re6("(^\\#)");
-	RegEx re7("(^$)");
-	vector<string> vs2,vr1,vr2;
-	string r1,r2;
-
-	while(getline(S, line)){
-		// File format: CMU \t SC SAMPA asciiN
-		chomp(line);
-		if (re6.Search(line)) continue;
-		if (re7.Search(line)) continue;
-		vs2=util::string2vector(line,"\t");
-		if (vs2.size()>2){
-			vr1=util::string2vector(vs2.at(1),"/");
-			if (vr1.size()>0) r1=vr1.at(0);
-			vr2=util::string2vector(vs2.at(2),"/");
-			if (vr2.size()>0) r2=vr2.at(0);
-			if ((vr2.size()>0) and (vr1.size()>0)){
-				SAMPA[r1]=r2;
-				
-			}
-		}
-	}
-	S.close();
-
-	// creating the soundchanguer
-	sc= new soundChange(vars,rules);
-
+      }
+    }
+  }
+  S.close();
+  
+  // creating the soundchanguer
+  sc= new soundChange(vars,rules);
+  
 }
 
 
@@ -159,7 +158,7 @@ phonetics::phonetics(string rulesFileIn, string dictFileIn, string sampaFileIn, 
 ///////////////////////////////////////////////////////////////
 
 phonetics::~phonetics(){
-	delete sc;
+  delete sc;
  
 }
 
@@ -168,33 +167,33 @@ phonetics::~phonetics(){
 /// getSound return the phonetic translation of a word
 /////////////////////////////////////////////////////////////////////////////
 string phonetics::getSound(string word){
-
-	string form=word;
-	string form1=form;
-	string sound;
-	
-	if (form1.empty()) return "";
-	sound=DIC[form1];
-	if (useDict and !sound.empty()) { return sound;}
-	else {
-		string aux=cache[form1];
-		if (aux.empty()){	
-			sound=sc->change(form1);
-			TRACE(4,"soundChange returns: "+sound+" from word "+form1);
-			string result;
-			for (size_t i=0; i<sound.size(); i++) {
-				string it=sound.substr(i,1);
-				string res=SAMPA[it];
-				result=result+res;
-			}
-			TRACE(4,"sampa translation converts "+sound+" into "+result);
-			sound=result;
-			cache[form1]=sound;
-		}
-		//Set
-		sound=cache[form1];
-	}
-	
-	return sound;
-
+  
+  string form=word;
+  string form1=form;
+  string sound;
+  
+  if (form1.empty()) return "";
+  sound=DIC[form1];
+  if (useDict and !sound.empty()) { return sound;}
+  else {
+    string aux=cache[form1];
+    if (aux.empty()){	
+      sound=sc->change(form1);
+      TRACE(4,"soundChange returns: "+sound+" from word "+form1);
+      string result;
+      for (size_t i=0; i<sound.size(); i++) {
+	string it=sound.substr(i,1);
+	string res=SAMPA[it];
+	result=result+res;
+      }
+      TRACE(4,"sampa translation converts "+sound+" into "+result);
+      sound=result;
+      cache[form1]=sound;
+    }
+    //Set
+    sound=cache[form1];
+  }
+  
+  return sound;
+  
 }
--- src/libmorfo/semdb.cc
+++ src/libmorfo/semdb.cc
@@ -37,15 +37,6 @@ using namespace std;
 #define MOD_TRACENAME "SEMDB"
 #define MOD_TRACECODE SENSES_TRACE
 
-// define apropriate open calls depending on Berkeley-DB version.
-#if (DB_VERSION_MAJOR==4 && DB_VERSION_MINOR==0)
-  #define OPEN(name,db,type,flags,mode)  open(name,db,type,flags,mode)
-#else
-#if DB_VERSION_MAJOR>=4
-  #define OPEN(name,db,type,flags,mode)  open(NULL,name,db,type,flags,mode)
-#endif
-#endif
-
 
 ///////////////////////////////////////////////////////////////
 ///  Constructor of the auxiliary class "sense_info"
@@ -79,28 +70,14 @@ string sense_info::get_parents_string() const {
   return util::list2string(parents,":");
 }
 
-
 ///////////////////////////////////////////////////////////////
 ///  Create the sense annotator
 ///////////////////////////////////////////////////////////////
 
-semanticDB::semanticDB(const std::string &SensesFile, const std::string &WNFile): sensesdb(NULL,DB_CXX_NO_EXCEPTIONS),wndb(NULL,DB_CXX_NO_EXCEPTIONS)
-{
-  int res;
+semanticDB::semanticDB(const std::string &SensesFile, const std::string &WNFile) {
 
-  // Opening a 4.0 or higher BerkeleyDB database
-  if (SensesFile!="") {
-    if ((res=sensesdb.OPEN(SensesFile.c_str(),NULL,DB_UNKNOWN,DB_RDONLY,0))) {
-      ERROR_CRASH("Error "+util::int2string(res)+" while opening database "+SensesFile);
-    }
-  }
-
-  // Opening a 4.0 or higher BerkeleyDB database
-  if (WNFile!="") {
-    if ((res=wndb.OPEN(WNFile.c_str(),NULL,DB_UNKNOWN,DB_RDONLY,0))) {
-      ERROR_CRASH("Error "+util::int2string(res)+" while opening database "+WNFile);
-    }
-  }
+  if (SensesFile!="") sensesdb.open_database(SensesFile.c_str());
+  if (WNFile!="") wndb.open_database(WNFile.c_str());
 
   TRACE(1,"module succesfully created");
 }
@@ -110,14 +87,9 @@ semanticDB::semanticDB(const std::string &SensesFile, const std::string &WNFile)
 ////////////////////////////////////////////////
 
 semanticDB::~semanticDB() {
-  int res;
-  //Close the database
-  if ((res=sensesdb.close(0))) {
-    ERROR_CRASH("Error "+util::int2string(res)+" while closing sense database");
-  }
-  if ((res=wndb.close(0))) {
-    ERROR_CRASH("Error "+util::int2string(res)+" while closing wn database");
-  }
+  //Close the databases
+  sensesdb.close_database();
+  wndb.close_database();
 }
 
 
@@ -126,7 +98,7 @@ semanticDB::~semanticDB() {
 ///////////////////////////////////////////////////////////////  
 
 list<string> semanticDB::get_word_senses(const string &lemma, const string &pos) {
-  return util::string2list(access_semantic_db(sensesdb,"W:"+lemma+":"+pos), " ");
+  return util::string2list(sensesdb.access_database("W:"+lemma+":"+pos), " ");
 }
 
 
@@ -135,7 +107,7 @@ list<string> semanticDB::get_word_senses(const string &lemma, const string &pos)
 ///////////////////////////////////////////////////////////////  
 
 list<string> semanticDB::get_sense_words(const string &sens, const string &pos) {
-  return util::string2list(access_semantic_db(sensesdb,"S:"+sens+":"+pos), " ");
+  return util::string2list(sensesdb.access_database("S:"+sens+":"+pos), " ");
 }
 
 
@@ -146,50 +118,9 @@ list<string> semanticDB::get_sense_words(const string &sens, const string &pos) 
 sense_info semanticDB::get_sense_info(const string &syn, const string &pos) {
   /// access DB and split obtained data_string into fields
   /// and store appropriately in a sense_info object
-  sense_info sinf(syn,pos, access_semantic_db(wndb,syn+":"+pos) );
+  sense_info sinf(syn,pos, wndb.access_database(syn+":"+pos) );
   /// add also list of synset words
   sinf.words=get_sense_words(syn,pos);
   return sinf;
-}
-
-
-//--------- Private functions --------//
-
-///////////////////////////////////////////////////////////////
-///  Get senses for a lemma+pos or words for a sense+pos
-///////////////////////////////////////////////////////////////  
-
-string semanticDB::access_semantic_db(Db &database, const string &clau) {
-  int error;
-  string data_string;
-  char buff[1024];
-  unsigned int p;
-  Dbt data, key;
-
-  // Access the DB
-  key.set_data((void *)clau.c_str());
-  key.set_size(clau.length());
-  error = database.get (NULL, &key, &data, 0);
-
-  list<string> lsen;
-  if (!error) {  // key found
-    // copy the data associated to the key to the buffer
-    p=data.get_size();
-    memcpy((void *)buff, data.get_data(), p);
-
-    // convert char* to string into data_string
-    buff[p]=0; data_string=buff;    
-
-    TRACE(3,clau+" Found: "+data_string);
-  }
-  else if (error == DB_NOTFOUND) {
-    TRACE(3,clau+" NOT found.");
-    data_string="";
-  }
-  else {
-    ERROR_CRASH("Error "+util::int2string(error)+" while accessing sense database");
-  }
-
-  return data_string;
 }
 
